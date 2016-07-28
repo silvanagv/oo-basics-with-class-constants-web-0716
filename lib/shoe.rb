da5ebc0a56149ac4c519d93d2aa7210e84636aa9
silvanagv <silvanagoberdhanvigle@gmail.com>
2016-07-28 10:35:50 -0400

    solved lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,16 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
+
 
   def cobble
     self.condition = "new"
